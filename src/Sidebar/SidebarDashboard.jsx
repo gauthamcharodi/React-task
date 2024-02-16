819554ac3ef96097c15772bbc873d5503c3510e4
gauthamcharodi <gauthamcharodi23@gmail.com>
2024-02-16 16:01:05 +0530

    task2

--- src/Sidebar/SidebarDashboard.jsx
+++ src/Sidebar/SidebarDashboard.jsx
@@ -3,9 +3,9 @@ import './Dashboard.css';
 import image from '../assests/download (1).png'
 
 const items = [
-  { id: image, channel: '#TKN20203754', orderDate: '2022-05-04', city: 'Lucknow', customerName: 'Abhishek Dixit', cost: 0.00, status: 'Pending', actions: 'Actions' },
-  { id: image, channel: '#TKN20203753', orderDate: '2022-05-04', city: 'Lucknow', customerName: 'Abhishek Dixit', cost: 0.00, status: 'Pending', actions: 'Actions' },
-  { id: image, channel: '#TKN20203752', orderDate: '2022-05-04', city: 'Lucknow', customerName: 'Abhishek Dixit', cost: 0.00, status: 'Pending', actions: 'Actions' },
+  { id: image, channel: '#TKN20203754', orderDate: '2022-05-04', city: 'Lucknow', customerName: 'Abhishek Dixit', orderValue:"0.00", status: 'Pending', actions: 'Actions' },
+  { id: image, channel: '#TKN20203753', orderDate: '2022-05-04', city: 'Lucknow', customerName: 'Abhishek Dixit', orderValue:"0.00", status: 'Pending', actions: 'Actions' },
+  { id: image, channel: '#TKN20203752', orderDate: '2022-05-04', city: 'Lucknow', customerName: 'Abhishek Dixit', orderValue:"0.00", status: 'Pending', actions: 'Actions' },
 ];
 
 const Dashboard = () => {
@@ -19,7 +19,7 @@ const Dashboard = () => {
             <th>Order Date</th>
             <th>City</th>
             <th>Customer Name</th>
-            <th>Cost</th>
+            <th>Order Value</th>
             <th>Status</th>
             <th>Operation</th>
           </tr>
@@ -32,7 +32,7 @@ const Dashboard = () => {
               <td>{item.orderDate}</td>
               <td>{item.city}</td>
               <td>{item.customerName}</td>
-              <td>{item.cost}</td>
+              <td>{item.orderValue}</td>
               <td>{item.status}</td>
               <td>{item.actions}</td>
             </tr>
